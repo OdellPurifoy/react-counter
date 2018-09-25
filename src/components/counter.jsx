4e7ee126e2f08a6f3f295b7b975a243639939d9a
Odell <odell.purifoy@gmail.com>
2018-09-25 13:41:45 -0400

    Added delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.value
   };
 
   // Use in case arrow function doesn't work for handleIncrement
@@ -12,7 +12,7 @@ class Counter extends Component {
   // }
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   doHandleIncrement = () => {
@@ -29,19 +29,20 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
 
   classButtonStyle() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
